ff580826592539ff95f5ab35efd5be8cd5121aac
dejan.stevanovic <dejan.stevanovic@primeholding.com>
2017-10-31 10:55:26 +0100

    Some hardcoded values moved to appsettings.json

--- ModernWallet/EmailSender.cs
+++ ModernWallet/EmailSender.cs
@@ -25,10 +25,9 @@ namespace ModernWallet
                     ApplicationSettings.Configuration["Email:Credentials:NetworkCredentials:Password"]);
 
             client.Send(
-                ApplicationSettings.Configuration["Email:Credentials:NetworkCredentials:UserName"],
-                //"dejan.stevanovic@primeholding.com", 
-                "elian.kurtenkov@primeholding.com",
-                "Recent Lykke registrations ", 
+               ApplicationSettings.Configuration["Email:Credentials:NetworkCredentials:UserName"],
+               ApplicationSettings.Configuration["Email:FeedbackRecipient"],
+               ApplicationSettings.Configuration["Email:FeedbackSubject"], 
                 feedback.Message);
         }
 
